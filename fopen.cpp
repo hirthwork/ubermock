a5d603586cd40b9d48e7608d947590219646a9fc
Dmitry Potapov <potapov.d@gmail.com>
2011-09-24 10:46:43 +0400

    Unified mocked function loading

--- fopen.cpp
+++ fopen.cpp
@@ -1,6 +1,7 @@
 #include <dlfcn.h>
 #include <stdio.h>
 #include <string.h>
+#include <reinvented-wheels/backtrace.hpp>
 
 FILE* fopen(const char* path, const char* mode)
 {
@@ -11,7 +12,8 @@ FILE* fopen(const char* path, const char* mode)
     else
     {
         typedef FILE* (*symbol)(const char*, const char*);
-        symbol sym = reinterpret_cast<symbol>(dlsym(RTLD_NEXT, "fopen"));
+        symbol sym = reinterpret_cast<symbol>(dlsym(RTLD_NEXT,
+            NReinventedWheels::GetCurrentFrame().Symbol_.c_str()));
         return sym(path, mode);
     }
 }

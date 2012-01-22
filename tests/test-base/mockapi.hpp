94a5a83948a49e2e442d3a89d78d297467fdc8f1
Dmitry Potapov <potapov.d@gmail.com>
2012-01-22 20:32:35 +0400

    Copyright notice added

--- tests/test-base/mockapi.hpp
+++ tests/test-base/mockapi.hpp
@@ -1,3 +1,22 @@
+/*
+ * mockapi.hpp              -- declaration of test functions to be mocked
+ *
+ * Copyright (C) 2012 Dmitry Potapov <potapov.d@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __MOCKAPI_HPP_2012_01_22__
 #define __MOCKAPI_HPP_2012_01_22__
 
--- tests/test-base/test-unary.cpp
+++ tests/test-base/test-unary.cpp
@@ -1,8 +1,27 @@
+/*
+ * test-unary.cpp           -- unary mocks tests
+ *
+ * Copyright (C) 2012 Dmitry Potapov <potapov.d@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <ubermock/registrar.hpp>
 
 #include "mockapi.hpp"
 
-#define BOOST_TEST_MODULE BinaryTest
+#define BOOST_TEST_MODULE UnaryTest
 #include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE(unary_original_values)

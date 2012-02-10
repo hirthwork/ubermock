c937f6e329b948e5550e1a2beac45f49779766af
Dmitry Potapov <potapov.d@gmail.com>
2012-02-10 09:49:52 +0400

    copyright notice added to isrange.hpp

--- ubermock/isrange.hpp
+++ ubermock/isrange.hpp
@@ -1,3 +1,22 @@
+/*
+ * isrange.hpp              -- range detecting trait
+ *
+ * Copyright (C) 2012 Dmitry Potapov <potapov.d@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __ISRANGE_HPP_2012_02_08__
 #define __ISRANGE_HPP_2012_02_08__
 
